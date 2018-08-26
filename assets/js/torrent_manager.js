0d4e227f1c8b553aa44a04df70132b5b98b1546b
Renan Rocha <renanoar04@gmail.com>
2018-08-25 23:36:34 -0300

    first time streaming video on browser

--- assets/js/torrent_manager.js
+++ assets/js/torrent_manager.js
@@ -2,10 +2,23 @@ var client = new WebTorrent();
     
 var magnetURI = $("#input_magnet_URI");
 var submitButton = $("#submit_button");
+var progress = $('#progress');
 
 submitButton.click(function() {
-    client.add(magnetURI.val(), function(torrent) {
-        alert(torrent.magnetURI);
-        // TODO iniciar download do torrent
-    });
+        client.on('error', function(err) {
+            alert('There is an error on client side: ' + err.message);
+        });
+        
+        client.add(magnetURI.val(), function(torrent) {
+            torrent.on('error', function(err) {
+                alert('There is an error reaching the torrent: ' + err.message);
+            });
+            var file = torrent.files.find(function (file) {
+                return file.name.endsWith('.mp4')
+            });
+            file.appendTo('#video-append', function(err, elem) {
+                if (err) throw err // file failed to download or display in the DOM
+                console.log('New DOM node with the content', elem)
+            });
+        });
 });
--- index.md
+++ index.md
@@ -7,3 +7,5 @@ layout: home
 
 <input id="input_magnet_URI" type="text" class="form-control" aria-describedby="emailHelp">
 <button type="button" id="submit_button" class="btn btn-primary">Start Streaming</button>
+<input id="progress" type="text">
+<div id="video-append" style="width: 80vw; height: 50vh;"></div>
